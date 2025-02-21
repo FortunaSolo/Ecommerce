f73014e104cb6e1ae6a115e71b6fbd4821a7b92d
Fortuna <fortunasolomon.fsm@gmail.com>
2025-02-22 00:22:02 +0300

    added DELETE request to the products

--- ecommerce-api/server/controllers/productController.js
+++ ecommerce-api/server/controllers/productController.js
@@ -1,5 +1,5 @@
 // server/controllers/productController.js
-
+const mongoose = require('mongoose');
 const Product = require('../models/Product');
 const Category = require('../models/Category');
 const Subcategory = require('../models/Subcategory');
@@ -93,6 +93,33 @@ const updateProduct = async (req, res) => {
   }
 };
 
+// Delete a product by ID
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // Check if ID is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: 'Invalid Product ID format' });
+    }
+
+    // Find the product and delete by ID
+    const product = await Product.findByIdAndDelete(id);
+    
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
 module.exports = {
-  getAllProducts, createProduct, updateProduct, // Add this line
+  getAllProducts, 
+  createProduct, 
+  updateProduct, 
+  deleteProduct,
 };
--- ecommerce-api/server/routes/products.js
+++ ecommerce-api/server/routes/products.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts, createProduct, updateProduct, } = require('../controllers/productController');
+const { 
+    getAllProducts, 
+    createProduct, 
+    updateProduct, 
+    deleteProduct, 
+} = require('../controllers/productController');
 
 // GET request to fetch all products
 router.get('/', getAllProducts);
@@ -10,5 +15,8 @@ router.post('/', createProduct);
 
 // PUT request for Updating a Product
 router.put('/:id', updateProduct);
+
+// DELETE Route for Deleting a Product
+router.delete('/:id', deleteProduct); // Add this route
 
 module.exports = router;
