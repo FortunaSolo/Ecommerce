a05dcc29bd3dc2a99e35114953d79869c6a4a9e1
Fortuna <fortunasolomon.fsm@gmail.com>
2025-02-26 17:39:35 +0300

    set front-end folder structure

--- ecommerce-api/server/index.js
+++ ecommerce-api/server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors');
+
+
 
 // Load environment variables from .env file
 dotenv.config();
@@ -13,6 +16,9 @@ const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Enable all CORS requests (adjust as necessary)
+app.use(cors()); 
 
 // Import routes
 const categoryRoutes = require('./routes/categories');
@@ -32,7 +38,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
