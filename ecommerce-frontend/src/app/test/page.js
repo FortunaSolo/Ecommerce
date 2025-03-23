22027a61104f3259e4d954a83c92bcae5d42bdd2
Fortuna <fortunasolomon.fsm@gmail.com>
2025-03-23 12:05:38 +0300

    removed cart to use call button

--- ecommerce-frontend/src/app/test/page.js
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-    return (
-      <div className="bg-blue-500 text-white p-4 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold">Hello, Tailwind with Next.js!</h1>
-        <p className="mt-2">If you see this, Tailwind is working correctly!</p>
-      </div>
-    );
-  }
-  
